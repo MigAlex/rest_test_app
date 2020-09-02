53d6751b229d5ea9361a8ebbd95fdd3421cea038
Alex <awangardowie@gmail.com>
2020-09-02 16:30:49 +0200

    12. class APIService v2

--- lib/app/services/api_service.dart
+++ lib/app/services/api_service.dart
@@ -1,17 +1,26 @@
-
+import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:ncov_rest_api_app/app/services/api.dart';
 
-class APIService{
+class APIService {
   final API api;
 
   APIService(this.api);
 
-  Future<String> getAccessToken() async{
+  Future<String> getAccessToken() async {
     final response = await http.post(
       api.tookenUri().toString(),
       headers: {'Authorization': 'Basic ${api.apiKey}'},
-      
     );
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      final accessToken = data['access_token'];
+      if (accessToken != null) {
+        return accessToken;
+      }
+    }
+    print(
+        'Request ${api.tookenUri()} failed \nResponse: ${response.statusCode} ${response.reasonPhrase}');
+    throw response;
   }
 }
