6753d3a130a57a9d866be07d5ffe86913bc5f697
Alex <awangardowie@gmail.com>
2020-09-04 18:58:12 +0200

    20. refreshing accessToken

--- lib/app/repositories/data_repository.dart
+++ lib/app/repositories/data_repository.dart
@@ -1,16 +1,29 @@
-
-
+import 'package:http/http.dart';
 import 'package:ncov_rest_api_app/app/services/api.dart';
 import 'package:ncov_rest_api_app/app/services/api_service.dart';
 import 'package:flutter/foundation.dart';
 
-class DataRepository{
+class DataRepository {
   final APIService apiService;
 
   DataRepository({@required this.apiService});
 
-  Future<int> getEndpointData(Endpoint endpoint) async{
-    final accessToken = await apiService.getAccessToken();
-    await apiService.getEndpointData(accessToken: accessToken, endpoint:  endpoint);
+  String _accessToken;
+
+  Future<int> getEndpointData(Endpoint endpoint) async {
+    try {
+      if (_accessToken == null) {
+        _accessToken = await apiService.getAccessToken();
+      }
+      return await apiService.getEndpointData(
+          accessToken: _accessToken, endpoint: endpoint);
+    } on Response catch (response) {
+      if (response.statusCode == 401) {
+        _accessToken = await apiService.getAccessToken();
+        return await apiService.getEndpointData(
+            accessToken: _accessToken, endpoint: endpoint);
+      }
+      rethrow;
+    }
   }
 }
